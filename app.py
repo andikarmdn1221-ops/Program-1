7080d0b9bac3dbba3c49c352b1ee004c41c5f7d6
andikarmdn1221-ops <andikarmdn1221@gmail.com>
2026-08-23 18:44:21 +0700

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ STOK_DEFAULT = {
     "Ceramic microcement": 4, "Microrock": 17, "Primer ordinary": 7,
     "Epoxy primer": 3, "Self leveling white finish": 4, "Top coat A": 15,
     "Top coat B": 1, "Top coat C": 5, "Pewarna no 1": 3,
-    "Pewarna no 2": 10, "Pewarna no 3": 0, "Pewarna no 4": 9, "Metal glaze wax": 0, "Mwtallic glaze wax": 0
+    "Pewarna no 2": 10, "Pewarna no 3": 0, "Pewarna no 4": 9, "Metal glaze wax": 0, "Metallic glaze wax": 0
 }
 
 # --- FUNGSI EFEK ANIMASI CONFETTI 🎉 ---
