982e30f0bf4ac0ac415af6f4f3b807e2fda7d0de
andikarmdn1221-ops <andikarmdn1221@gmail.com>
2026-08-23 18:45:56 +0700

    Update app.py

--- app.py
+++ app.py
@@ -11,13 +11,14 @@ st.set_page_config(page_title="Microcement Warehouse", page_icon="📦", layout=
 # --- URL GOOGLE APPS SCRIPT KAMU ---
 URL_GSHEET_API = "https://script.google.com/macros/s/AKfycbyudM_n5g9O2S88pconh7dJHp0oeEJ0D400dG26wKkysNazniISvSXbNT5ArWL_xY04jg/exec"
 
-# --- DATA DEFAULT AWAL ---
+# --- DATA DEFAULT AWAL (SUDAH DITAMBAHKAN METALLIC GLAZE WAX) ---
 STOK_DEFAULT = {
     "Microcement base": 16, "Ready to use": 15, "Mixed resin A": 12,
     "Ceramic microcement": 4, "Microrock": 17, "Primer ordinary": 7,
     "Epoxy primer": 3, "Self leveling white finish": 4, "Top coat A": 15,
     "Top coat B": 1, "Top coat C": 5, "Pewarna no 1": 3,
-    "Pewarna no 2": 10, "Pewarna no 3": 0, "Pewarna no 4": 9, "Metal glaze wax": 0, "Metallic glaze wax": 0
+    "Pewarna no 2": 10, "Pewarna no 3": 0, "Pewarna no 4": 9, 
+    "Metal glaze wax": 0, "Metallic glaze wax": 0
 }
 
 # --- FUNGSI EFEK ANIMASI CONFETTI 🎉 ---
@@ -103,7 +104,7 @@ def save_data():
 if "stok" not in st.session_state or "riwayat" not in st.session_state:
     st.session_state.stok, st.session_state.riwayat = load_data()
 
-st.title("📦 Sistem Gudang Microcement")
+st.title("📦 Sistem Gudang Mikrosemen")
 
 menu = st.sidebar.selectbox("Pilih Menu", [
     "📊 Lihat Semua Stok", 
@@ -180,7 +181,7 @@ if menu == "📊 Lihat Semua Stok":
             st.plotly_chart(fig_bar, use_container_width=True)
             
         with col_chart2:
-            st.markdown("##### 📦 Proporsi Status Stok Gudang")
+            st.markdown("##### 🥧 Proporsi Status Stok Gudang")
             fig_pie = px.pie(
                 df_stok, names="StatusGrafik", color="StatusGrafik",
                 color_discrete_map={"AMAN": "#2ecc71", "KRITIS": "#f1c40f", "HABIS!": "#e74c3c"},
