81dea6e6d6c1446ec16045e491fcac108c000079
andikarmdn1221-ops <andikarmdn1221@gmail.com>
2026-08-24 19:54:03 +0700

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ from fpdf import FPDF
 st.set_page_config(page_title="Microcement Warehouse", page_icon="📦", layout="wide")
 
 URL_GSHEET_API = "https://script.google.com/macros/s/AKfycbyudM_n5g9O2S88pconh7dJHp0oeEJ0D400dG26wKkysNazniISvSXbNT5ArWL_xY04jg/exec"
-TELEGRAM_BOT_TOKEN = "8849647370:AAESRwPya7DVJAYR7WgvxL8eESqIV81ZqpE"
+TELEGRAM_BOT_TOKEN = "8936505684:AAGM3KuPnq8u88Y3HMDKmvBHkQuthsq9bwI"
 TELEGRAM_CHAT_ID = 2106196278
 
 def kirim_notifikasi_telegram(pesan):
