4d3888a476a3b8db65746417f4b8f4244964628f
andikarmdn1221-ops <andikarmdn1221@gmail.com>
2026-08-24 18:42:59 +0700

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ st.set_page_config(page_title="Microcement Warehouse", page_icon="📦", layout=
 # --- 1. KONFIGURASI URL & TOKEN TELEGRAM LANGSUNG ---
 URL_GSHEET_API = "https://script.google.com/macros/s/AKfycbyudM_n5g9O2S88pconh7dJHp0oeEJ0D400dG26wKkysNazniISvSXbNT5ArWL_xY04jg/exec"
 
-TELEGRAM_BOT_TOKEN = "8810239918:AAGBfJH1gOUc4d4172bpqhaaoMYORiJUl0gw"
+TELEGRAM_BOT_TOKEN = "8849647370: AAESRwPya7DVJAYR7Wgv xL8eESqIV8lzQpE"
 TELEGRAM_CHAT_ID = 2106196278
 
 def kirim_notifikasi_telegram(pesan):
